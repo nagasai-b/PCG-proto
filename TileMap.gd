06d3bcb36ed71c365988c756113fb06d547e8018
Faris Ahmed T. Alotaibi <faa339@nyu.edu>
2022-03-08 13:40:13 -0500

    Basic astar implemented
    TODO: prevent overlapping paths (or clearly specify direction), connect path in full circle

--- TileMap.gd
+++ TileMap.gd
@@ -4,20 +4,30 @@ extends TileMap
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
-var width = 64
-var height = 39
-var grid = []
+var astar = AStar2D.new()
+var width = 32
+var height = 19
+var grid = {}
 var generated = false
 var rng = RandomNumberGenerator.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	rng.randomize()
+	astar.reserve_space(width*height)
+	var pointID = 1
 	for i in range(width):
-		grid.append([])
 		for j in range(height):
-			grid[i].append(0)
-
+			set_cell(i, j, 0)
+			grid[Vector2(i,j)] = pointID
+			astar.add_point(pointID, Vector2(i,j), 1.0)
+			pointID+=1 
+	var neighbors = [Vector2(0,-1),Vector2(0,1),Vector2(1,0), Vector2(-1, 0)]
+	
+	for pt in grid.keys():
+		for n in neighbors:
+			if grid.has(pt+n):
+				astar.connect_points(grid[pt], grid[pt+n], false)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if generated==false:
@@ -27,26 +37,17 @@ func _process(delta):
 
 
 func create_map():
-	var temp_points = []
+	var temp_points = PoolVector2Array()
 	for i in range(5):
-		var x = rng.randf_range(0, width)
-		var y = rng.randf_range(0, height)
-		grid[x][y] = 1
-		temp_points.append([x,y])
-	
-	var center = [0, 0]
-	
-	for i in temp_points:
-		center[0] += i[0]
-		center[1] += i[1]
-	
-	center[0] = floor(center[0]/5)
-	center[1] =	floor(center[1]/5)
-	
-	grid[center[0]][center[1]] = 1
-	
-	for i in range(width):
-		for j in range(height):
-			set_cell(i,j,grid[i][j]);
+		var x = floor(rng.randf_range(0, width))
+		var y = floor(rng.randf_range(0, height))
+		set_cell(x,y,1)
+		temp_points.append(Vector2(x,y))
+	for p in range(1,5):
+		var p1 = grid[temp_points[p-1]]
+		var p2 = grid[temp_points[p]]
+		var path = astar.get_point_path(p1,p2)
+		for line in path:
+			set_cell(line.x,line.y,1)
+		pass
 	pass
-	
