8e6b52e76489893636c52208ab7857a49171e0a6
Naga <nb3052@nyu.edu>
2022-03-10 15:34:15 -0500

    Starts new level when falls

--- scripts/TileTrack.gd
+++ scripts/TileTrack.gd
@@ -29,7 +29,6 @@ func _ready():
 	print(map_seed)
 	tile_size = cell_size
 	make_maze()
-	translate_to_world()
 
 
 func make_maze():
@@ -67,12 +66,6 @@ func make_maze():
 		elif stack:
 			current = stack.pop_back()
 	goal = last_positions[randi()%last_positions.size()]
-
-
-func translate_to_world():
-#	Finally, we translate the tilemap coordinates to world space (so that we can
-#	use them for later calculations) and put them into an array. We also paint 
-#	our road markers back onto the map.
-	set_cellv(goal, 16)
+	set_cellv(goal, 21)
 	goal = map_to_world(goal, false) + cell_size/2
-	print(goal)
+
